f656a0c797228ad5a294e2db20288f7b78b7bba1
walkercz <lastovka.cz@gmail.com>
2020-06-24 15:00:26 +0200

    added include.js to all pages

---
+++ js/include.js
@@ -0,0 +1,8 @@
+
+$(function(){
+    var includes = $('[data-include]');
+    jQuery.each(includes, function(){
+        var file = $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
